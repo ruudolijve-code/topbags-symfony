b890b3fc598499411c6a9ce25dd08923bca0c9ce
ruudtopbags <ruud.olijve@topbags.nl>
2026-04-23 10:47:51 +0200

    Verplaats mail afzenders naar env configuratie

--- src/Twig/AppGlobals.php
+++ src/Twig/AppGlobals.php
@@ -7,14 +7,16 @@ use Twig\Extension\GlobalsInterface;
 final class AppGlobals implements GlobalsInterface
 {
     public function __construct(
-        private string $ga4MeasurementId
+        private string $ga4MeasurementId,
+        private string $appUrl,
     ) {
     }
- 
+
     public function getGlobals(): array
     {
         return [
             'ga4_measurement_id' => $this->ga4MeasurementId,
+            'app_url' => rtrim($this->appUrl, '/'),
         ];
     }
 }
