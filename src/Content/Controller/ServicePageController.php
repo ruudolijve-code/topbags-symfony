2a1127566175aa3057433644830e6b74ce6594b1
ruudtopbags <ruud.olijve@topbags.nl>
2026-05-05 11:40:40 +0200

    Verplaats mail afzenders naar env configuratie

--- src/Content/Controller/ServicePageController.php
+++ src/Content/Controller/ServicePageController.php
@@ -20,33 +20,45 @@ final class ServicePageController extends AbstractController
         return $this->render('service/shipping.html.twig');
     }
 
+    #[Route('/betaalmethoden', name: 'service_payment_methods', methods: ['GET'])]
+    public function paymentMethods(): Response
+    {
+        return $this->render('service/payment_methods.html.twig');
+    }
+
     #[Route('/garantie-reparatie', name: 'service_warranty', methods: ['GET'])]
     public function warranty(): Response
     {
         return $this->render('service/warranty.html.twig');
     }
 
-    #[Route('/service/reparatie', name: 'service_repair')]
+    #[Route('/service/reparatie', name: 'service_repair', methods: ['GET'])]
     public function repair(): Response
     {
-        return $this->render('service/repair.html.twig');
+        return $this->redirectToRoute('service_warranty', [], Response::HTTP_MOVED_PERMANENTLY);
     }
 
-    #[Route('/service/privacy', name: 'service_privacy')]
+    #[Route('/algemene-voorwaarden', name: 'service_terms', methods: ['GET'])]
+    public function terms(): Response
+    {
+        return $this->render('service/terms.html.twig');
+    }
+
+    #[Route('/service/privacy', name: 'service_privacy', methods: ['GET'])]
     public function privacy(): Response
     {
         return $this->render('service/privacy.html.twig');
     }
 
-    #[Route('/service/cookies', name: 'service_cookies')]
+    #[Route('/service/cookies', name: 'service_cookies', methods: ['GET'])]
     public function cookies(): Response
     {
         return $this->render('service/cookies.html.twig');
     }
 
-     #[Route('/winkel', name: 'service_store')]
-     public function store(): Response
-     {
+    #[Route('/winkel', name: 'service_store', methods: ['GET'])]
+    public function store(): Response
+    {
         return $this->render('service/store.html.twig');
-     }
+    }
 }
