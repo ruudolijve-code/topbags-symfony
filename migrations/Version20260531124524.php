cecdef8d3d8ed1e59785a75aba32e48d66d93495
ruudtopbags <ruud.olijve@topbags.nl>
2026-05-31 14:47:45 +0200

    Voeg merk en leverancierskleur toe aan orderitems

---
+++ migrations/Version20260531124524.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260531124524 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE order_item ADD brand_name VARCHAR(255) DEFAULT NULL, ADD supplier_color_name VARCHAR(255) DEFAULT NULL, CHANGE variant_sku variant_sku VARCHAR(100) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE order_item DROP brand_name, DROP supplier_color_name, CHANGE variant_sku variant_sku VARCHAR(255) NOT NULL');
+    }
+}
