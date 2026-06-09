eb496ffccf60c4d6ec1c80cdc0cb5406365940b4
ruudtopbags <ruud.olijve@topbags.nl>
2026-06-09 15:21:15 +0200

    Verplaats mail afzenders naar env configuratie

--- src/Admin/Controller/ProductVariantCrudController.php
+++ src/Admin/Controller/ProductVariantCrudController.php
@@ -5,7 +5,6 @@ namespace App\Admin\Controller;
 use App\Catalog\Entity\Image;
 use App\Catalog\Entity\ProductVariant;
 use App\Catalog\Service\VariantImageUploader;
-use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
@@ -14,24 +13,24 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
 use EasyCorp\Bundle\EasyAdminBundle\Field\FormField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\SlugField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
-use EasyCorp\Bundle\EasyAdminBundle\Field\SlugField;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
-use Symfony\Contracts\Cache\CacheInterface;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Contracts\Cache\CacheInterface;
 
-#[IsGranted('ROLE_ADMIN')]
-
+#[IsGranted('ROLE_STORE')]
 class ProductVariantCrudController extends AbstractCrudController
 {
     public function __construct(
-        private CacheInterface $cache,
-        private VariantImageUploader $variantImageUploader
+        private readonly CacheInterface $cache,
+        private readonly VariantImageUploader $variantImageUploader,
     ) {
     }
 
@@ -60,74 +59,101 @@ class ProductVariantCrudController extends AbstractCrudController
 
     public function configureActions(Actions $actions): Actions
     {
-        return $actions
+        $actions = $actions
             ->add(Crud::PAGE_INDEX, Action::DETAIL);
+
+        if (!$this->isGranted('ROLE_ADMIN')) {
+            $actions = $actions
+                ->disable(Action::NEW, Action::DELETE);
+        }
+
+        return $actions;
     }
 
     public function configureFields(string $pageName): iterable
     {
+        $storeOnly = !$this->isGranted('ROLE_ADMIN');
+
         yield FormField::addPanel('Variant');
 
-        yield AssociationField::new('product', 'Product');
+        yield AssociationField::new('product', 'Product')
+            ->setFormTypeOption('disabled', $storeOnly);
 
         yield TextField::new('product.brand.name', 'Merk')
-        ->onlyOnIndex();
+            ->onlyOnIndex();
 
-        yield TextField::new('variantSku', 'SKU');
+        yield TextField::new('variantSku', 'SKU')
+            ->setFormTypeOption('disabled', $storeOnly);
 
         yield TextField::new('ean', 'EAN')
-            ->hideOnIndex();
+            ->hideOnIndex()
+            ->setFormTypeOption('disabled', $storeOnly);
 
         yield FormField::addPanel('Prijs & sale');
 
         yield MoneyField::new('price', 'Normale prijs')
             ->setCurrency('EUR')
-            ->setStoredAsCents(false);
+            ->setStoredAsCents(false)
+            ->setFormTypeOption('disabled', $storeOnly);
 
         yield MoneyField::new('compareAtPrice', 'Adviesprijs')
             ->setCurrency('EUR')
             ->setStoredAsCents(false)
-            ->hideOnIndex();
+            ->hideOnIndex()
+            ->setFormTypeOption('disabled', $storeOnly);
 
         yield IntegerField::new('salePercentage', 'Sale %')
-            ->hideOnIndex();
+            ->hideOnIndex()
+            ->setFormTypeOption('disabled', $storeOnly);
 
         yield DateTimeField::new('saleStartsAt', 'Sale start')
-            ->hideOnIndex();
+            ->hideOnIndex()
+            ->setFormTypeOption('disabled', $storeOnly);
 
         yield DateTimeField::new('saleEndsAt', 'Sale einde')
-            ->hideOnIndex();
+            ->hideOnIndex()
+            ->setFormTypeOption('disabled', $storeOnly);
 
         yield TextField::new('saleLabel', 'Sale label')
-            ->hideOnIndex();
+            ->hideOnIndex()
+            ->setFormTypeOption('disabled', $storeOnly);
 
-        yield BooleanField::new('isMaster', 'Master');
+        yield BooleanField::new('isMaster', 'Master')
+            ->setFormTypeOption('disabled', $storeOnly);
 
-        yield BooleanField::new('isActive', 'Actief');
+        yield BooleanField::new('isActive', 'Actief')
+            ->setFormTypeOption('disabled', $storeOnly);
 
         yield FormField::addPanel('Kleur');
 
         yield AssociationField::new('color', 'Kleur')
-            ->hideOnIndex();
+            ->hideOnIndex()
+            ->setFormTypeOption('disabled', $storeOnly);
 
         yield AssociationField::new('normalizedColor', 'Genormaliseerde kleur')
-            ->hideOnIndex();
+            ->hideOnIndex()
+            ->setFormTypeOption('disabled', $storeOnly);
 
         yield TextField::new('supplierColorName', 'Supplier kleur')
-            ->hideOnIndex();
+            ->hideOnIndex()
+            ->setFormTypeOption('disabled', $storeOnly);
 
         yield SlugField::new('supplierColorSlug', 'Supplier kleur slug')
-            ->setTargetFieldName('supplierColorName');    
+            ->setTargetFieldName('supplierColorName')
+            ->setFormTypeOption('disabled', $storeOnly);
 
         yield TextField::new('supplierColorCode', 'Supplier kleurcode')
-            ->hideOnIndex();
+            ->hideOnIndex()
+            ->setFormTypeOption('disabled', $storeOnly);
 
         yield FormField::addPanel('Voorraad');
 
         yield IntegerField::new('stockOnHand', 'Op voorraad');
 
         yield IntegerField::new('stockReserved', 'Gereserveerd')
-            ->hideOnIndex();
+            ->hideOnIndex()
+            ->setFormTypeOption('disabled', $storeOnly)
+            ->setHelp('Gereserveerde voorraad wordt normaal door bestellingen bepaald.');
 
         yield IntegerField::new('stockAvailable', 'Beschikbaar')
             ->onlyOnIndex();
@@ -135,26 +161,28 @@ class ProductVariantCrudController extends AbstractCrudController
         yield BooleanField::new('allowBackorder', 'Backorder toegestaan')
             ->renderAsSwitch(false);
 
-        yield FormField::addPanel('Afbeeldingen');
+        if ($this->isGranted('ROLE_ADMIN')) {
+            yield FormField::addPanel('Afbeeldingen');
 
-        yield Field::new('uploadedImages', 'Afbeeldingen uploaden')
-            ->setFormType(FileType::class)
-            ->setFormTypeOptions([
-                'multiple' => true,
-                'required' => false,
-                'mapped' => true,
-                'attr' => [
-                    'accept' => 'image/*',
-                ],
-            ])
-            ->onlyOnForms();
+            yield Field::new('uploadedImages', 'Afbeeldingen uploaden')
+                ->setFormType(FileType::class)
+                ->setFormTypeOptions([
+                    'multiple' => true,
+                    'required' => false,
+                    'mapped' => true,
+                    'attr' => [
+                        'accept' => 'image/*',
+                    ],
+                ])
+                ->onlyOnForms();
 
-        yield CollectionField::new('images', 'Bestaande afbeeldingen')
-            ->useEntryCrudForm(ImageCrudController::class)
-            ->setFormTypeOption('allow_add', false)
-            ->setFormTypeOption('allow_delete', true)
-            ->setHelp('Nieuwe afbeeldingen voeg je hierboven toe via upload.')
-            ->hideOnIndex();
+            yield CollectionField::new('images', 'Bestaande afbeeldingen')
+                ->useEntryCrudForm(ImageCrudController::class)
+                ->setFormTypeOption('allow_add', false)
+                ->setFormTypeOption('allow_delete', true)
+                ->setHelp('Nieuwe afbeeldingen voeg je hierboven toe via upload.')
+                ->hideOnIndex();
+        }
     }
 
     public function persistEntity(EntityManagerInterface $entityManager, $entityInstance): void
@@ -163,6 +191,10 @@ class ProductVariantCrudController extends AbstractCrudController
             parent::persistEntity($entityManager, $entityInstance);
 
             return;
+        }
+
+        if (!$this->isGranted('ROLE_ADMIN')) {
+            throw $this->createAccessDeniedException('Winkelmedewerkers mogen geen nieuwe varianten aanmaken.');
         }
 
         parent::persistEntity($entityManager, $entityInstance);
@@ -181,7 +213,10 @@ class ProductVariantCrudController extends AbstractCrudController
 
         parent::updateEntity($entityManager, $entityInstance);
 
-        $this->handleUploadedImages($entityManager, $entityInstance);
+        if ($this->isGranted('ROLE_ADMIN')) {
+            $this->handleUploadedImages($entityManager, $entityInstance);
+        }
+
         $this->invalidateAvailabilityCache($entityInstance);
     }
 
