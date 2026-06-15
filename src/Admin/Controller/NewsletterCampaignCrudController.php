c80eef2b7a177e7991b3bf6b51b03e6b2323be59
ruudtopbags <ruud.olijve@topbags.nl>
2026-06-15 20:23:20 +0200

    Verplaats mail afzenders naar env configuratie

--- src/Admin/Controller/NewsletterCampaignCrudController.php
+++ src/Admin/Controller/NewsletterCampaignCrudController.php
@@ -5,10 +5,12 @@ declare(strict_types=1);
 namespace App\Admin\Controller;
 
 use App\Marketing\Entity\NewsletterCampaign;
+use App\Marketing\Repository\NewsletterDeliveryRepository;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
+use EasyCorp\Bundle\EasyAdminBundle\Config\KeyValueStore;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
@@ -25,6 +27,7 @@ final class NewsletterCampaignCrudController extends AbstractCrudController
 {
     public function __construct(
         private readonly AdminUrlGenerator $adminUrlGenerator,
+        private readonly NewsletterDeliveryRepository $deliveryRepository,
     ) {
     }
 
@@ -38,25 +41,70 @@ final class NewsletterCampaignCrudController extends AbstractCrudController
         return $crud
             ->setEntityLabelInSingular('Nieuwsbrief')
             ->setEntityLabelInPlural('Nieuwsbrieven')
-            ->setPageTitle(Crud::PAGE_INDEX, 'Nieuwsbrieven')
-            ->setPageTitle(Crud::PAGE_NEW, 'Nieuwe nieuwsbrief')
+            ->setPageTitle(
+                Crud::PAGE_INDEX,
+                'Nieuwsbrieven'
+            )
+            ->setPageTitle(
+                Crud::PAGE_NEW,
+                'Nieuwe nieuwsbrief'
+            )
             ->setPageTitle(
                 Crud::PAGE_EDIT,
-                static fn (NewsletterCampaign $campaign): string => sprintf(
+                static fn (
+                    NewsletterCampaign $campaign
+                ): string => sprintf(
                     'Nieuwsbrief bewerken: %s',
                     $campaign->getTitle()
                 )
             )
             ->setPageTitle(
                 Crud::PAGE_DETAIL,
-                static fn (NewsletterCampaign $campaign): string => sprintf(
+                static fn (
+                    NewsletterCampaign $campaign
+                ): string => sprintf(
                     'Nieuwsbrief: %s',
                     $campaign->getTitle()
                 )
             )
             ->setDefaultSort([
                 'createdAt' => 'DESC',
-            ]);
+            ])
+            ->overrideTemplate(
+                'crud/detail',
+                'admin/newsletter_campaign/detail.html.twig'
+            );
+    }
+
+    public function configureResponseParameters(
+        KeyValueStore $responseParameters
+    ): KeyValueStore {
+        if (
+            $responseParameters->get('pageName')
+            !== Crud::PAGE_DETAIL
+        ) {
+            return $responseParameters;
+        }
+
+        $entityDto = $responseParameters->get('entity');
+
+        if ($entityDto === null) {
+            return $responseParameters;
+        }
+
+        $campaign = $entityDto->getInstance();
+
+        if (!$campaign instanceof NewsletterCampaign) {
+            return $responseParameters;
+        }
+
+        $responseParameters->set(
+            'deliveryStatistics',
+            $this->deliveryRepository
+                ->getStatisticsForCampaign($campaign)
+        );
+
+        return $responseParameters;
     }
 
     public function configureActions(Actions $actions): Actions
@@ -66,14 +114,21 @@ final class NewsletterCampaignCrudController extends AbstractCrudController
             'Preview',
             'fa fa-eye'
         )
-            ->linkToUrl(function (NewsletterCampaign $campaign): string {
-                return $this->adminUrlGenerator
-                    ->unsetAll()
-                    ->setRoute('admin_newsletter_campaign_preview', [
-                        'id' => $campaign->getId(),
-                    ])
-                    ->generateUrl();
-            })
+            ->linkToUrl(
+                function (
+                    NewsletterCampaign $campaign
+                ): string {
+                    return $this->adminUrlGenerator
+                        ->unsetAll()
+                        ->setRoute(
+                            'admin_newsletter_campaign_preview',
+                            [
+                                'id' => $campaign->getId(),
+                            ]
+                        )
+                        ->generateUrl();
+                }
+            )
             ->setHtmlAttributes([
                 'target' => '_blank',
                 'rel' => 'noopener noreferrer',
@@ -84,31 +139,46 @@ final class NewsletterCampaignCrudController extends AbstractCrudController
             'Testmail',
             'fa fa-envelope'
         )
-            ->linkToUrl(function (NewsletterCampaign $campaign): string {
-                return $this->adminUrlGenerator
-                    ->unsetAll()
-                    ->setRoute('admin_newsletter_campaign_test_mail', [
-                        'id' => $campaign->getId(),
-                    ])
-                    ->generateUrl();
-            });
+            ->linkToUrl(
+                function (
+                    NewsletterCampaign $campaign
+                ): string {
+                    return $this->adminUrlGenerator
+                        ->unsetAll()
+                        ->setRoute(
+                            'admin_newsletter_campaign_test_mail',
+                            [
+                                'id' => $campaign->getId(),
+                            ]
+                        )
+                        ->generateUrl();
+                }
+            );
 
         $bulkTest = Action::new(
             'bulkTestNewsletter',
             'Bulktest',
             'fa fa-users'
         )
-            ->linkToUrl(function (NewsletterCampaign $campaign): string {
-                return $this->adminUrlGenerator
-                    ->unsetAll()
-                    ->setRoute('admin_newsletter_campaign_bulk_test', [
-                        'id' => $campaign->getId(),
-                    ])
-                    ->generateUrl();
-            })
+            ->linkToUrl(
+                function (
+                    NewsletterCampaign $campaign
+                ): string {
+                    return $this->adminUrlGenerator
+                        ->unsetAll()
+                        ->setRoute(
+                            'admin_newsletter_campaign_bulk_test',
+                            [
+                                'id' => $campaign->getId(),
+                            ]
+                        )
+                        ->generateUrl();
+                }
+            )
             ->displayIf(
-                static fn (NewsletterCampaign $campaign): bool =>
-                    $campaign->isDraft()
+                static fn (
+                    NewsletterCampaign $campaign
+                ): bool => $campaign->isDraft()
             )
             ->addCssClass('btn btn-warning');
 
@@ -117,17 +187,25 @@ final class NewsletterCampaignCrudController extends AbstractCrudController
             'Nieuwsbrief versturen',
             'fa fa-paper-plane'
         )
-            ->linkToUrl(function (NewsletterCampaign $campaign): string {
-                return $this->adminUrlGenerator
-                    ->unsetAll()
-                    ->setRoute('admin_newsletter_campaign_send', [
-                        'id' => $campaign->getId(),
-                    ])
-                    ->generateUrl();
-            })
+            ->linkToUrl(
+                function (
+                    NewsletterCampaign $campaign
+                ): string {
+                    return $this->adminUrlGenerator
+                        ->unsetAll()
+                        ->setRoute(
+                            'admin_newsletter_campaign_send',
+                            [
+                                'id' => $campaign->getId(),
+                            ]
+                        )
+                        ->generateUrl();
+                }
+            )
             ->displayIf(
-                static fn (NewsletterCampaign $campaign): bool =>
-                    $campaign->isDraft()
+                static fn (
+                    NewsletterCampaign $campaign
+                ): bool => $campaign->isDraft()
             )
             ->addCssClass('btn btn-danger');
 
@@ -153,8 +231,7 @@ final class NewsletterCampaignCrudController extends AbstractCrudController
              * Bewerkpagina
              *
              * De bulk- en definitieve verzendknoppen staan hier bewust
-             * niet. Zo moet de nieuwsbrief eerst worden opgeslagen voordat
-             * meerdere of definitieve e-mails worden ingepland.
+             * niet. De nieuwsbrief moet eerst worden opgeslagen.
              */
             ->add(Crud::PAGE_EDIT, $preview)
             ->add(Crud::PAGE_EDIT, $testMail)
@@ -166,17 +243,23 @@ final class NewsletterCampaignCrudController extends AbstractCrudController
             ->update(
                 Crud::PAGE_INDEX,
                 Action::EDIT,
-                static fn (Action $action): Action => $action->displayIf(
-                    static fn (NewsletterCampaign $campaign): bool =>
-                        $campaign->isDraft()
+                static fn (
+                    Action $action
+                ): Action => $action->displayIf(
+                    static fn (
+                        NewsletterCampaign $campaign
+                    ): bool => $campaign->isDraft()
                 )
             )
             ->update(
                 Crud::PAGE_DETAIL,
                 Action::EDIT,
-                static fn (Action $action): Action => $action->displayIf(
-                    static fn (NewsletterCampaign $campaign): bool =>
-                        $campaign->isDraft()
+                static fn (
+                    Action $action
+                ): Action => $action->displayIf(
+                    static fn (
+                        NewsletterCampaign $campaign
+                    ): bool => $campaign->isDraft()
                 )
             )
 
@@ -198,30 +281,45 @@ final class NewsletterCampaignCrudController extends AbstractCrudController
     {
         yield FormField::addTab('Inhoud');
 
-        yield TextField::new('title', 'Interne titel')
+        yield TextField::new(
+            'title',
+            'Interne titel'
+        )
             ->setHelp(
                 'Alleen zichtbaar in admin, bijvoorbeeld: Voorjaarsactie koffers mei 2026.'
             );
 
-        yield TextField::new('subject', 'Onderwerpregel')
+        yield TextField::new(
+            'subject',
+            'Onderwerpregel'
+        )
             ->setHelp(
                 'Dit wordt de onderwerpregel van de e-mail.'
             );
 
-        yield TextField::new('preheader', 'Preheader')
+        yield TextField::new(
+            'preheader',
+            'Preheader'
+        )
             ->setRequired(false)
             ->setHelp(
                 'Korte previewtekst die sommige e-mailclients naast of onder het onderwerp tonen.'
             );
 
-        yield TextareaField::new('htmlBody', 'HTML inhoud')
+        yield TextareaField::new(
+            'htmlBody',
+            'HTML inhoud'
+        )
             ->onlyOnForms()
             ->setNumOfRows(24)
             ->setHelp(
                 'Gebruik eenvoudige HTML met inline styles. Geen Twig-code gebruiken.'
             );
 
-        yield TextField::new('emailPreview', 'E-mailpreview')
+        yield TextField::new(
+            'emailPreview',
+            'E-mailpreview'
+        )
             ->onlyOnDetail()
             ->setTemplatePath(
                 'admin/field/newsletter_preview.html.twig'
@@ -232,7 +330,10 @@ final class NewsletterCampaignCrudController extends AbstractCrudController
         yield IdField::new('id')
             ->onlyOnDetail();
 
-        yield ChoiceField::new('status', 'Status')
+        yield ChoiceField::new(
+            'status',
+            'Status'
+        )
             ->setChoices([
                 'Concept' => NewsletterCampaign::STATUS_DRAFT,
                 'Wordt verzonden' => NewsletterCampaign::STATUS_SENDING,
@@ -243,22 +344,54 @@ final class NewsletterCampaignCrudController extends AbstractCrudController
                 NewsletterCampaign::STATUS_SENDING => 'warning',
                 NewsletterCampaign::STATUS_SENT => 'success',
             ])
-            ->setFormTypeOption('disabled', true);
+            ->setFormTypeOption(
+                'disabled',
+                true
+            );
 
-        yield IntegerField::new('recipientCount', 'Ontvangers')
-            ->setFormTypeOption('disabled', true);
+        yield IntegerField::new(
+            'recipientCount',
+            'Ontvangers'
+        )
+            ->setFormTypeOption(
+                'disabled',
+                true
+            );
 
-        yield IntegerField::new('sentCount', 'Verzonden')
-            ->setFormTypeOption('disabled', true);
+        yield IntegerField::new(
+            'sentCount',
+            'SMTP geaccepteerd'
+        )
+            ->setHelp(
+                'Aantal berichten dat door de uitgaande mailserver is geaccepteerd.'
+            )
+            ->setFormTypeOption(
+                'disabled',
+                true
+            );
 
-        yield IntegerField::new('failedCount', 'Mislukt')
-            ->setFormTypeOption('disabled', true);
+        yield IntegerField::new(
+            'failedCount',
+            'Direct mislukt'
+        )
+            ->setHelp(
+                'Aantal berichten dat al tijdens het verzendproces direct is mislukt.'
+            )
+            ->setFormTypeOption(
+                'disabled',
+                true
+            );
 
-        yield DateTimeField::new('createdAt', 'Aangemaakt op')
+        yield DateTimeField::new(
+            'createdAt',
+            'Aangemaakt op'
+        )
             ->hideOnForm();
 
-        yield DateTimeField::new('sentAt', 'Verzonden op')
+        yield DateTimeField::new(
+            'sentAt',
+            'Verwerking afgerond op'
+        )
             ->hideOnForm();
     }
 }
-
--- src/Marketing/Repository/NewsletterDeliveryRepository.php
+++ src/Marketing/Repository/NewsletterDeliveryRepository.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Marketing\Repository;
 
+use App\Marketing\Entity\NewsletterCampaign;
 use App\Marketing\Entity\NewsletterDelivery;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
@@ -86,5 +87,103 @@ final class NewsletterDeliveryRepository extends ServiceEntityRepository
             ->getOneOrNullResult();
 
         return $delivery;
+    }
+
+    /**
+     * @return array{
+     *     total: int,
+     *     queued: int,
+     *     smtpAccepted: int,
+     *     likelyDelivered: int,
+     *     directFailed: int,
+     *     hardBounce: int,
+     *     softBounce: int,
+     *     technicalFailure: int,
+     *     review: int,
+     *     deliveryRate: float
+     * }
+     */
+    public function getStatisticsForCampaign(
+        NewsletterCampaign $campaign
+    ): array {
+        /** @var array<string, int|string|null> $result */
+        $result = $this->createQueryBuilder('delivery')
+            ->select('COUNT(delivery.id) AS total')
+            ->addSelect(
+                'SUM(CASE WHEN delivery.status = :queued THEN 1 ELSE 0 END) AS queued'
+            )
+            ->addSelect(
+                'SUM(CASE WHEN delivery.smtpAcceptedAt IS NOT NULL THEN 1 ELSE 0 END) AS smtpAccepted'
+            )
+            ->addSelect(
+                'SUM(CASE WHEN delivery.status = :smtpAcceptedStatus THEN 1 ELSE 0 END) AS likelyDelivered'
+            )
+            ->addSelect(
+                'SUM(CASE WHEN delivery.status = :directFailed THEN 1 ELSE 0 END) AS directFailed'
+            )
+            ->addSelect(
+                'SUM(CASE WHEN delivery.status = :hardBounce THEN 1 ELSE 0 END) AS hardBounce'
+            )
+            ->addSelect(
+                'SUM(CASE WHEN delivery.status = :softBounce THEN 1 ELSE 0 END) AS softBounce'
+            )
+            ->addSelect(
+                'SUM(CASE WHEN delivery.status = :technicalFailure THEN 1 ELSE 0 END) AS technicalFailure'
+            )
+            ->addSelect(
+                'SUM(CASE WHEN delivery.status = :review THEN 1 ELSE 0 END) AS review'
+            )
+            ->andWhere('delivery.campaign = :campaign')
+            ->setParameter('campaign', $campaign)
+            ->setParameter(
+                'queued',
+                NewsletterDelivery::STATUS_QUEUED
+            )
+            ->setParameter(
+                'smtpAcceptedStatus',
+                NewsletterDelivery::STATUS_SMTP_ACCEPTED
+            )
+            ->setParameter(
+                'directFailed',
+                NewsletterDelivery::STATUS_DIRECT_FAILED
+            )
+            ->setParameter(
+                'hardBounce',
+                NewsletterDelivery::STATUS_HARD_BOUNCE
+            )
+            ->setParameter(
+                'softBounce',
+                NewsletterDelivery::STATUS_SOFT_BOUNCE
+            )
+            ->setParameter(
+                'technicalFailure',
+                NewsletterDelivery::STATUS_TECHNICAL_FAILURE
+            )
+            ->setParameter(
+                'review',
+                NewsletterDelivery::STATUS_REVIEW
+            )
+            ->getQuery()
+            ->getSingleResult();
+
+        $total = (int) ($result['total'] ?? 0);
+        $likelyDelivered = (int) ($result['likelyDelivered'] ?? 0);
+
+        return [
+            'total' => $total,
+            'queued' => (int) ($result['queued'] ?? 0),
+            'smtpAccepted' => (int) ($result['smtpAccepted'] ?? 0),
+            'likelyDelivered' => $likelyDelivered,
+            'directFailed' => (int) ($result['directFailed'] ?? 0),
+            'hardBounce' => (int) ($result['hardBounce'] ?? 0),
+            'softBounce' => (int) ($result['softBounce'] ?? 0),
+            'technicalFailure' => (int) (
+                $result['technicalFailure'] ?? 0
+            ),
+            'review' => (int) ($result['review'] ?? 0),
+            'deliveryRate' => $total > 0
+                ? round(($likelyDelivered / $total) * 100, 1)
+                : 0.0,
+        ];
     }
 }
