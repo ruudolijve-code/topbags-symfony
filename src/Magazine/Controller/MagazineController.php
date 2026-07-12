fe103f2fda7dc2f560a45c00cdd7a62a1bee2047
ruudtopbags <ruud.olijve@topbags.nl>
2026-07-12 11:34:27 +0200

    Verplaats mail afzenders naar env configuratie

--- src/Magazine/Controller/MagazineController.php
+++ src/Magazine/Controller/MagazineController.php
@@ -15,32 +15,105 @@ use Symfony\Component\Routing\Attribute\Route;
 final class MagazineController extends AbstractController
 {
     #[Route('/magazine', name: 'magazine_index', methods: ['GET'])]
-    public function index(
+    public function shopIndex(
         MagazineArticleRepository $articles,
     ): Response {
-        $context = MagazineArticle::CONTEXT_SHOP;
+        return $this->renderMagazineIndex(
+            MagazineArticle::CONTEXT_SHOP,
+            $articles
+        );
+    }
 
-        $featured = $articles->findFeaturedByContext($context);
-
-        return $this->render('magazine/index.html.twig', [
-            'featured' => $featured,
-            'articles' => $articles->findPublishedByContextExceptFeatured(
-                $context,
-                $featured
-            ),
-            'context' => $context,
-        ]);
+    #[Route('/bags/magazine', name: 'bags_magazine_index', methods: ['GET'])]
+    public function bagsIndex(
+        MagazineArticleRepository $articles,
+    ): Response {
+        return $this->renderMagazineIndex(
+            MagazineArticle::CONTEXT_BAGS,
+            $articles
+        );
     }
 
     #[Route('/magazine/{slug}', name: 'magazine_show', methods: ['GET'])]
-    public function show(
+    public function shopShow(
         string $slug,
         MagazineArticleRepository $articles,
         VariantImagePathResolver $imagePathResolver,
         LightweightSuitcaseProvider $lightweightSuitcaseProvider,
     ): Response {
-        $context = MagazineArticle::CONTEXT_SHOP;
+        return $this->renderMagazineArticle(
+            $slug,
+            MagazineArticle::CONTEXT_SHOP,
+            $articles,
+            $imagePathResolver,
+            $lightweightSuitcaseProvider
+        );
+    }
 
+    #[Route(
+        '/bags/magazine/{slug}',
+        name: 'bags_magazine_show',
+        methods: ['GET']
+    )]
+    public function bagsShow(
+        string $slug,
+        MagazineArticleRepository $articles,
+        VariantImagePathResolver $imagePathResolver,
+        LightweightSuitcaseProvider $lightweightSuitcaseProvider,
+    ): Response {
+        return $this->renderMagazineArticle(
+            $slug,
+            MagazineArticle::CONTEXT_BAGS,
+            $articles,
+            $imagePathResolver,
+            $lightweightSuitcaseProvider
+        );
+    }
+
+    private function renderMagazineIndex(
+        string $context,
+        MagazineArticleRepository $articles,
+    ): Response {
+        $featured = $articles->findFeaturedByContext($context);
+
+        $magazine = match ($context) {
+            MagazineArticle::CONTEXT_BAGS => [
+                'pageTitle' => 'Topbags Fashionmagazine | Tassenadvies & inspiratie',
+                'label' => 'Topbags Fashionmagazine',
+                'heading' => 'Tassenadvies, trends en inspiratie',
+                'intro' => 'Ontdek alles over damestassen, rugzakken, laptoptassen, portemonnees, accessoires, materialen en onderhoud. Praktisch advies en inspiratie van de tassenspecialist.',
+                'showRoute' => 'bags_magazine_show',
+                'emptyMessage' => 'Er zijn nog geen artikelen over tassen en accessoires gepubliceerd.',
+            ],
+
+            default => [
+                'pageTitle' => 'Topbags Reismagazine | Kofferadvies & reistips',
+                'label' => 'Topbags Reismagazine',
+                'heading' => 'Kofferadvies, handbagageregels en reistips',
+                'intro' => 'Praktische uitleg van de kofferspecialist van Twente. Ontdek alles over koffers, handbagage, vliegmaatschappijen, reparaties, verhuur en slimme reistips.',
+                'showRoute' => 'magazine_show',
+                'emptyMessage' => 'Er zijn nog geen reisartikelen gepubliceerd.',
+            ],
+        };
+
+        return $this->render('magazine/index.html.twig', [
+            'context' => $context,
+            'magazine' => $magazine,
+            'featured' => $featured,
+            'articles' => $articles->findPublishedByContextExceptFeatured(
+                $context,
+                $featured
+            ),
+        ]);
+    }
+
+    private function renderMagazineArticle(
+        string $slug,
+        string $context,
+        MagazineArticleRepository $articles,
+        VariantImagePathResolver $imagePathResolver,
+        LightweightSuitcaseProvider $lightweightSuitcaseProvider,
+    ): Response {
         $article = $articles->findPublishedBySlugWithRelations(
             $slug,
             $context
@@ -81,8 +154,14 @@ final class MagazineController extends AbstractController
             'article' => $article,
             'context' => $context,
             'relatedProductItems' => $relatedProductItems,
-            'lightweightSuitcaseItems' =>
-                $lightweightSuitcaseProvider->getItems(8),
+
+            /*
+             * Alleen relevant voor het koffermagazine.
+             * De Twig-template controleert daarnaast nog op de slug.
+             */
+            'lightweightSuitcaseItems' => $context === MagazineArticle::CONTEXT_SHOP
+                ? $lightweightSuitcaseProvider->getItems(8)
+                : [],
         ]);
     }
 }
