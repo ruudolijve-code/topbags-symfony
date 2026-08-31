d746a603e1bf62415f6de9884fda4d97cd2d3373
ruudtopbags <ruud.olijve@topbags.nl>
2026-08-31 09:52:07 +0200

    Fix EasyAdmin filters for Style Guide productoverride affinities

--- src/Admin/Controller/StyleGuideProductOverrideCrudController.php
+++ src/Admin/Controller/StyleGuideProductOverrideCrudController.php
@@ -13,31 +13,63 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
-use EasyCorp\Bundle\EasyAdminBundle\Filter\AssociationFilter;
 use EasyCorp\Bundle\EasyAdminBundle\Filter\BooleanFilter;
 use EasyCorp\Bundle\EasyAdminBundle\Filter\NumericFilter;
 
 final class StyleGuideProductOverrideCrudController extends AbstractCrudController
 {
-    public static function getEntityFqcn(): string { return StyleGuideProductOverride::class; }
+    public static function getEntityFqcn(): string
+    {
+        return StyleGuideProductOverride::class;
+    }
+
     public function configureCrud(Crud $crud): Crud
     {
-        return $crud->setEntityLabelInSingular('Productuitzondering')->setEntityLabelInPlural('Productuitzonderingen')
-            ->setDefaultSort(['styleWorld.position' => 'ASC', 'id' => 'DESC'])
-            ->setSearchFields(['product.name', 'product.modelSku', 'styleWorld.name', 'reason']);
+        return $crud
+            ->setEntityLabelInSingular('Productuitzondering')
+            ->setEntityLabelInPlural('Productuitzonderingen')
+            ->setDefaultSort([
+                'styleWorld.position' => 'ASC',
+                'id' => 'DESC',
+            ])
+            ->setSearchFields([
+                'product.name',
+                'product.modelSku',
+                'styleWorld.name',
+                'reason',
+            ]);
     }
+
     public function configureFilters(Filters $filters): Filters
     {
-        return $filters->add(AssociationFilter::new('styleWorld'))->add(AssociationFilter::new('product'))
-            ->add(NumericFilter::new('scoreAdjustment'))->add(BooleanFilter::new('isActive'));
+        return $filters
+            ->add('styleWorld')
+            ->add('product')
+            ->add(NumericFilter::new('scoreAdjustment', 'Scorecorrectie'))
+            ->add(BooleanFilter::new('isActive', 'Actief'));
     }
+
     public function configureFields(string $pageName): iterable
     {
-        yield IdField::new('id')->hideOnForm();
+        yield IdField::new('id')
+            ->hideOnForm();
+
         yield AssociationField::new('styleWorld', 'Stijlwereld');
-        yield AssociationField::new('product', 'Product')->autocomplete();
-        yield IntegerField::new('scoreAdjustment', 'Scorecorrectie')->setHelp('Alleen voor uitzonderingen; positief bevoordeelt en negatief verlaagt.');
-        yield TextField::new('reason', 'Reden')->setRequired(false);
+
+        yield AssociationField::new('product', 'Product')
+            ->autocomplete();
+
+        yield IntegerField::new('scoreAdjustment', 'Scorecorrectie')
+            ->setHelp(
+                'Alleen voor uitzonderingen; een positieve waarde bevoordeelt het product en een negatieve waarde verlaagt de score.'
+            );
+
+        yield TextField::new('reason', 'Reden')
+            ->setRequired(false)
+            ->setHelp(
+                'Optionele toelichting waarom dit product binnen deze stijlwereld een afwijkende score krijgt.'
+            );
+
         yield BooleanField::new('isActive', 'Actief');
     }
 }
